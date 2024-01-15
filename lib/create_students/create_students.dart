26422bc33f4960c5752be725c29d0a90d269dc64
Amawers <dacoroon.christianjericho@gmail.com>
2024-01-15 11:17:16 +0800

    done integration frontend subject list, student list, create student, and their integration to backend

--- lib/create_students/create_students.dart
+++ lib/create_students/create_students.dart
@@ -1,19 +1,57 @@
+import 'dart:convert';
+import 'package:attendifyyy/api_connection/api_connection.dart';
+import 'package:attendifyyy/authentication/user_preferences/user_preferences.dart';
+import 'package:attendifyyy/bottom_nav_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class ListOfStudentsScreen extends StatefulWidget {
   String? subject_name;
   String? subject_code;
   String? section_id;
   String? subject_id;
-  ListOfStudentsScreen({required this.subject_name, required this.subject_code, required this.section_id, required this.subject_id});
+  ListOfStudentsScreen(
+      {required this.subject_name,
+      required this.subject_code,
+      required this.section_id,
+      required this.subject_id});
 
   @override
   State<ListOfStudentsScreen> createState() => _ListOfStudentsScreenState();
 }
 
 class _ListOfStudentsScreenState extends State<ListOfStudentsScreen> {
-
   List<dynamic> studentList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getListOfStudents();
+  }
+
+  Future<void> getListOfStudents() async {
+    Map<String, dynamic>? teacherInfo =
+        await RememberUserPreferences.readUserInfo();
+
+    String teacherId = teacherInfo?['teacher_id'];
+
+    Map<String, String> headers = {
+      'subject_name': widget.subject_name ?? '',
+      'section_id': widget.section_id ?? '',
+      'subject_id': widget.subject_id ?? '',
+      'teacher_id': teacherId
+    };
+    final response =
+        await http.get(Uri.parse(Api.listOfStudents), headers: headers);
+    if (response.statusCode == 200) {
+      setState(() {
+        studentList = jsonDecode(response.body);
+      });
+      print("Sa student list ni ${studentList}");
+    } else {
+      print("nag error conenct sa backend");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,16 +66,19 @@ class _ListOfStudentsScreenState extends State<ListOfStudentsScreen> {
           itemCount: studentList.length,
           itemBuilder: (context, index) {
             return ListOfStudentsWidget(
-                subject_name:
-                    studentList[index]['first_name'] ?? 'students Null nia, e edit lang',
-                subject_code:
-                    studentList[index]['last_name'] ?? 'students Null nia, e edit lang');
+                first_name: studentList[index]['first_name'] ?? 'No fname',
+                last_name: studentList[index]['last_name'] ?? 'No lname',
+                grade_level:
+                    studentList[index]['grade_level'] ?? 'No grade level');
           }),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          // showDialog(
-          //     context: context,
-          //     builder: (BuildContext context) => Dialog(child: CreateStudent(section_id: widget.section_id, subject_id: widget.subject_id)));
+          showDialog(
+              context: context,
+              builder: (BuildContext context) => Dialog(
+                  child: CreateStudent(
+                      section_id: widget.section_id,
+                      subject_id: widget.subject_id)));
         },
         child: const Text('Add Student',
             style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold)),
@@ -47,19 +88,116 @@ class _ListOfStudentsScreenState extends State<ListOfStudentsScreen> {
 }
 
 class ListOfStudentsWidget extends StatelessWidget {
-  String subject_name;
-  String subject_code;
+  String first_name;
+  String last_name;
+  String grade_level;
 
-  ListOfStudentsWidget({required this.subject_name, required this.subject_code});
+  ListOfStudentsWidget(
+      {required this.first_name,
+      required this.last_name,
+      required this.grade_level});
 
   @override
   Widget build(BuildContext context) {
-    return  Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(subject_name),
-              const SizedBox(width: 50),
-              Text(subject_code)
-            ]);
+    return Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+      Column(
+        children: [
+          Text(first_name),
+          const SizedBox(width: 50),
+          Text(last_name),
+        ],
+      ),
+      const SizedBox(width: 50),
+      Text(grade_level)
+    ]);
+  }
+}
+
+class CreateStudent extends StatelessWidget {
+  String? section_id;
+  String? subject_id;
+  CreateStudent({required this.section_id, required this.subject_id});
+
+  TextEditingController referenceNumberController = TextEditingController();
+  TextEditingController firstNameController = TextEditingController();
+  TextEditingController middleInitialController = TextEditingController();
+  TextEditingController lastNameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController courseController = TextEditingController();
+  TextEditingController gradeLevelController = TextEditingController();
+
+  Future<void> createStudent() async {
+    final response = await http.post(Uri.parse(Api.createStudent), body: {
+      'reference_number': referenceNumberController.text,
+      'first_name': firstNameController.text,
+      'middle_initial': middleInitialController.text,
+      'last_name': lastNameController.text,
+      'email': emailController.text,
+      'course': courseController.text,
+      'grade_level': gradeLevelController.text,
+      'section_id': section_id,
+      'subject_id': subject_id
+    });
+
+    if (response.statusCode == 200) {
+      print(response.body);
+    } else {
+      print("nag error conenct sa backend");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      child: ListView(
+        children: [
+          const Text('Create Class'),
+          Form(
+              child: Column(children: [
+            TextFormField(
+              controller: referenceNumberController,
+              decoration: const InputDecoration(hintText: 'Reference Number'),
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              controller: firstNameController,
+              decoration: const InputDecoration(hintText: 'First Name'),
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              controller: middleInitialController,
+              decoration: const InputDecoration(hintText: 'Middle Initial'),
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              controller: lastNameController,
+              decoration: const InputDecoration(hintText: 'Last Name'),
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              controller: emailController,
+              decoration: const InputDecoration(hintText: 'Email'),
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              controller: courseController,
+              decoration: const InputDecoration(hintText: 'Course'),
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              controller: gradeLevelController,
+              decoration: const InputDecoration(hintText: 'Grade Level'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+                onPressed: () {
+                  createStudent();
+                },
+                child: const Text("Create"))
+          ]))
+        ],
+      ),
+    );
   }
 }
--- new.sql
+++ new.sql
@@ -30,3 +30,24 @@ CREATE TABLE subject_teachers (
     constraint subject_teachers_subject_id_fkey foreign key (subject_id) references subjects (subject_id),
     constraint subject_teachers_teacher_id_fkey foreign key (teacher_id) references teachers (teacher_id)
 );
+
+CREATE TABLE students (
+    student_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(255),
+    email VARCHAR(255),
+    grade_level VARCHAR(255),
+    last_name VARCHAR(255),
+    reference_number VARCHAR(255),
+    middle_initial VARCHAR(255),
+    course VARCHAR(255)
+) 
+
+CREATE TABLE student_subjects (
+    student_subjects_id INT AUTO_INCREMENT PRIMARY KEY,
+    student_id INT NOT NULL,
+    subject_id INT NOT NULL,
+    section_id INT NOT NULL,
+    constraint student_subjects_section_id_fkey foreign key (section_id) references sections (section_id),
+    constraint student_subjects_student_id_fkey foreign key (student_id) references students (student_id),
+    constraint student_subjects_subject_id_fkey foreign key (subject_id) references subjects (subject_id)
+)
