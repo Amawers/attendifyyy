cb93767db33395386531779b7644c8f785ab73ba
Amawers <dacoroon.christianjericho@gmail.com>
2024-01-23 22:10:09 +0800

    folder restructure for scan_qr folder

---
+++ lib/scanning_qr/scan_qr_overlay.dart
@@ -0,0 +1,54 @@
+import 'package:attendifyyy/scanning_qr/utility_widgets/border_painter.dart';
+import 'package:flutter/material.dart';
+
+class ScanQrOverlay extends StatelessWidget {
+  const ScanQrOverlay({Key? key, required this.overlayColour})
+      : super(key: key);
+
+  final Color overlayColour;
+
+  @override
+  Widget build(BuildContext context) {
+    double scanArea = (MediaQuery.of(context).size.width < 400 ||
+            MediaQuery.of(context).size.height < 400)
+        ? 300.0
+        : 430.0;
+    return Stack(children: [
+      ColorFiltered(
+        colorFilter: ColorFilter.mode(
+            overlayColour, BlendMode.srcOut), // This one will create the magic
+        child: Stack(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                  color: Colors.red,
+                  backgroundBlendMode: BlendMode
+                      .dstOut), // This one will handle background + difference out
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+                height: scanArea,
+                width: scanArea,
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      Align(
+        alignment: Alignment.center,
+        child: CustomPaint(
+          foregroundPainter: BorderPainter(),
+          child: SizedBox(
+            width: scanArea + 20,
+            height: scanArea + 20,
+          ),
+        ),
+      ),
+    ]);
+  }
+}
